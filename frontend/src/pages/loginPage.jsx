e08887d1e9c19045fcf1434597f6f9b4af2e367a
jmanero <ingmanerojoaquin@gmail.com>
2023-11-15 23:39:11 -0300

    tomate

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,134 +1,169 @@
 import React, { useState, useEffect } from "react";
-import { Button, Typography, Container, TextField } from "@mui/material";
-import { Link } from "react-router-dom";
+//import { Typography, Container, TextField } from "@mui/material";
+import axios from "axios";
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  //const [animationClass, setAnimationClass] = useState("");
 
-  const [animationClass, setAnimationClass] = useState("");
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
-  //Crear funcion que se llame handleLogin, donde si el email es igual a admin y el password es igual a admin, entonces redirigir a la pagina de bienvenida
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleLogin = () => {
-    if (username === "admin@gmail.com" && password === "admin") {
-      console.log("Login successful");
-      window.location.href = "/welcomePage";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/v1/auth/autenticar",
+        credentials
+      );
+      const { token, refreshToken } = response.data;
+
+      // Store the tokens in localStorage or secure cookie for later use
+      localStorage.setItem("token", token);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      // Redirect or perform other actions upon successful login
+    } catch (error) {
+      // Handle login error
     }
   };
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setAnimationClass("appear");
-    }, 100);
-    return () => clearTimeout(timeout);
-  }, []);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setAnimationClass("appear");
+  //   }, 100);
+  //   return () => clearTimeout(timeout);
+  // }, []);
 
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        background: `
-    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${process.env.PUBLIC_URL}/fotoLandingPage.jpg)
-  `,
-        backgroundSize: "cover",
-        color: "#fff",
-        textAlign: "center",
-        minWidth: "100%",
-      }}
-    >
-      <Typography
-        variant="h2"
-        sx={{
-          fontSize: "2.5rem",
-          marginBottom: "20px",
-          opacity: 0,
-          transform: "translateY(20px)",
-          transition: "opacity 0.8s ease-in-out, transform 0.8s ease-in-out",
-          "&.appear": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
-        }}
-        className={animationClass}
-      >
-        ¡Bienvenido a nuestro sistema educativo!
-      </Typography>
-      <Typography
-        sx={{
-          opacity: 0,
-          transform: "translateY(20px)",
-          transition: "opacity 0.8s ease-in-out, transform 1.4s ease-in-out",
-          "&.appear": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
-        }}
-        className={animationClass}
-      >
-        Explora todas las características de nuestro sistema utilizando JSON Web
-        Tokens.
-      </Typography>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}
-      >
-        <TextField
-          label="Email"
-          variant="outlined"
-          sx={{
-            marginBottom: "10px",
-            backgroundColor: "gray",
-            color: "#fff",
-            transform: "translateY(20px)",
-            transition: "opacity 0.8s ease-in-out, transform 1.8s ease-in-out",
-            "&.appear": {
-              transform: "translateY(0)",
-            },
-          }}
-          value={username}
-          className={animationClass}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          variant="outlined"
-          sx={{
-            marginBottom: "10px",
-            color: "#fff",
-            backgroundColor: "gray",
-            transform: "translateY(20px)",
-            transition: "opacity 0.8s ease-in-out, transform 2.0s ease-in-out",
-            "&.appear": {
-              transform: "translateY(0)",
-            },
-          }}
-          type="password"
-          value={password}
-          className={animationClass}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <Button
-          onClick={handleLogin}
-          variant="contained"
-          sx={{ marginTop: "20px", backgroundColor: "#f44336", color: "#fff" }}
-        >
-          Login
-        </Button>
-      </form>
-    </Container>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        value={credentials.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={credentials.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
+
+  // return (
+  //   <Container
+  //     sx={{
+  //       display: "flex",
+  //       flexDirection: "column",
+  //       alignItems: "center",
+  //       justifyContent: "center",
+  //       minHeight: "100vh",
+  //       background: `
+  //   linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+  //   url(${process.env.PUBLIC_URL}/fotoLandingPage.jpg)
+  // `,
+  //       backgroundSize: "cover",
+  //       color: "#fff",
+  //       textAlign: "center",
+  //       minWidth: "100%",
+  //     }}
+  //   >
+  //     <Typography
+  //       variant="h2"
+  //       sx={{
+  //         fontSize: "2.5rem",
+  //         marginBottom: "20px",
+  //         opacity: 0,
+  //         transform: "translateY(20px)",
+  //         transition: "opacity 0.8s ease-in-out, transform 0.8s ease-in-out",
+  //         "&.appear": {
+  //           opacity: 1,
+  //           transform: "translateY(0)",
+  //         },
+  //       }}
+  //       className={animationClass}
+  //     >
+  //       ¡Bienvenido a nuestro sistema educativo!
+  //     </Typography>
+  //     <Typography
+  //       sx={{
+  //         opacity: 0,
+  //         transform: "translateY(20px)",
+  //         transition: "opacity 0.8s ease-in-out, transform 1.4s ease-in-out",
+  //         "&.appear": {
+  //           opacity: 1,
+  //           transform: "translateY(0)",
+  //         },
+  //       }}
+  //       className={animationClass}
+  //     >
+  //       Explora todas las características de nuestro sistema utilizando JSON Web
+  //       Tokens.
+  //     </Typography>
+  //     <form
+  //       onSubmit={handleSubmit}
+  //       style={{
+  //         display: "flex",
+  //         flexDirection: "column",
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //         marginTop: "20px",
+  //       }}
+  //     >
+  //       <TextField
+  //         label="Email"
+  //         variant="outlined"
+  //         sx={{
+  //           marginBottom: "10px",
+  //           backgroundColor: "gray",
+  //           color: "#fff",
+  //           transform: "translateY(20px)",
+  //           transition: "opacity 0.8s ease-in-out, transform 1.8s ease-in-out",
+  //           "&.appear": {
+  //             transform: "translateY(0)",
+  //           },
+  //         }}
+  //         value={credentials.email}
+  //         className={animationClass}
+  //         onChange={handleChange}
+  //       />
+  //       <TextField
+  //         label="Password"
+  //         variant="outlined"
+  //         sx={{
+  //           marginBottom: "10px",
+  //           color: "#fff",
+  //           backgroundColor: "gray",
+  //           transform: "translateY(20px)",
+  //           transition: "opacity 0.8s ease-in-out, transform 2.0s ease-in-out",
+  //           "&.appear": {
+  //             transform: "translateY(0)",
+  //           },
+  //         }}
+  //         type="password"
+  //         value={credentials.password}
+  //         className={animationClass}
+  //         onChange={handleChange}
+  //       />
+
+  //       <button type="submit">Login</button>
+  //     </form>
+  //   </Container>
+  // );
 };
 
 export default LoginPage;
--- spring-api/src/main/java/ar/com/lupibuddies/springapi/config/SecurityConfiguration.java
+++ spring-api/src/main/java/ar/com/lupibuddies/springapi/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static ar.com.lupibuddies.springapi.usuario.enums.Permiso.*;
 import static ar.com.lupibuddies.springapi.usuario.enums.Rol.ADMIN;
@@ -44,9 +50,21 @@ public class SecurityConfiguration {
     private final LogoutHandler logoutHandler;
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(""));
+        configuration.setAllowedMethods(List.of(""));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
@@ -65,8 +83,7 @@ public class SecurityConfiguration {
                         logout.logoutUrl("/v1/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-                )
-        ;
+                );
 
         return http.build();
     }

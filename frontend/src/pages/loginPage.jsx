2ac72ef5977653efb1dc8817b2e3b4cdad5a4cd6
jmanero <ingmanerojoaquin@gmail.com>
2023-11-16 00:37:51 -0300

    cambios en frontend con login hecho

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-//import { Typography, Container, TextField } from "@mui/material";
+import { Typography, Container, TextField } from "@mui/material";
 import axios from "axios";
 
 const LoginPage = () => {
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
-  //const [animationClass, setAnimationClass] = useState("");
+  const [animationClass, setAnimationClass] = useState("");
 
   const [credentials, setCredentials] = useState({
     email: "",
@@ -27,143 +27,147 @@ const LoginPage = () => {
         "http://localhost:8080/v1/auth/autenticar",
         credentials
       );
-      const { token, refreshToken } = response.data;
+      const { access_token, refresh_token } = response.data;
 
       // Store the tokens in localStorage or secure cookie for later use
-      localStorage.setItem("token", token);
-      localStorage.setItem("refreshToken", refreshToken);
-
-      // Redirect or perform other actions upon successful login
+      localStorage.setItem("token", access_token);
+      localStorage.setItem("refreshToken", refresh_token);
+      console.log("token:", access_token);
+      console.log(refresh_token);
     } catch (error) {
       // Handle login error
     }
   };
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setAnimationClass("appear");
-  //   }, 100);
-  //   return () => clearTimeout(timeout);
-  // }, []);
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        value={credentials.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={credentials.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimationClass("appear");
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, []);
 
   // return (
-  //   <Container
-  //     sx={{
-  //       display: "flex",
-  //       flexDirection: "column",
-  //       alignItems: "center",
-  //       justifyContent: "center",
-  //       minHeight: "100vh",
-  //       background: `
-  //   linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-  //   url(${process.env.PUBLIC_URL}/fotoLandingPage.jpg)
-  // `,
-  //       backgroundSize: "cover",
-  //       color: "#fff",
-  //       textAlign: "center",
-  //       minWidth: "100%",
-  //     }}
-  //   >
-  //     <Typography
-  //       variant="h2"
-  //       sx={{
-  //         fontSize: "2.5rem",
-  //         marginBottom: "20px",
-  //         opacity: 0,
-  //         transform: "translateY(20px)",
-  //         transition: "opacity 0.8s ease-in-out, transform 0.8s ease-in-out",
-  //         "&.appear": {
-  //           opacity: 1,
-  //           transform: "translateY(0)",
-  //         },
-  //       }}
-  //       className={animationClass}
-  //     >
-  //       ¡Bienvenido a nuestro sistema educativo!
-  //     </Typography>
-  //     <Typography
-  //       sx={{
-  //         opacity: 0,
-  //         transform: "translateY(20px)",
-  //         transition: "opacity 0.8s ease-in-out, transform 1.4s ease-in-out",
-  //         "&.appear": {
-  //           opacity: 1,
-  //           transform: "translateY(0)",
-  //         },
-  //       }}
-  //       className={animationClass}
-  //     >
-  //       Explora todas las características de nuestro sistema utilizando JSON Web
-  //       Tokens.
-  //     </Typography>
-  //     <form
-  //       onSubmit={handleSubmit}
-  //       style={{
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         alignItems: "center",
-  //         justifyContent: "center",
-  //         marginTop: "20px",
-  //       }}
-  //     >
-  //       <TextField
-  //         label="Email"
-  //         variant="outlined"
-  //         sx={{
-  //           marginBottom: "10px",
-  //           backgroundColor: "gray",
-  //           color: "#fff",
-  //           transform: "translateY(20px)",
-  //           transition: "opacity 0.8s ease-in-out, transform 1.8s ease-in-out",
-  //           "&.appear": {
-  //             transform: "translateY(0)",
-  //           },
-  //         }}
-  //         value={credentials.email}
-  //         className={animationClass}
-  //         onChange={handleChange}
-  //       />
-  //       <TextField
-  //         label="Password"
-  //         variant="outlined"
-  //         sx={{
-  //           marginBottom: "10px",
-  //           color: "#fff",
-  //           backgroundColor: "gray",
-  //           transform: "translateY(20px)",
-  //           transition: "opacity 0.8s ease-in-out, transform 2.0s ease-in-out",
-  //           "&.appear": {
-  //             transform: "translateY(0)",
-  //           },
-  //         }}
-  //         type="password"
-  //         value={credentials.password}
-  //         className={animationClass}
-  //         onChange={handleChange}
-  //       />
-
-  //       <button type="submit">Login</button>
-  //     </form>
-  //   </Container>
+  //   <form onSubmit={handleSubmit}>
+  //     <input
+  //       type="email"
+  //       name="email"
+  //       value={credentials.email}
+  //       onChange={handleChange}
+  //     />
+  //     <input
+  //       type="password"
+  //       name="password"
+  //       value={credentials.password}
+  //       onChange={handleChange}
+  //     />
+  //     <button type="submit">Login</button>
+  //   </form>
   // );
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        background: `
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${process.env.PUBLIC_URL}/fotoLandingPage.jpg)
+  `,
+        backgroundSize: "cover",
+        color: "#fff",
+        textAlign: "center",
+        minWidth: "100%",
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: "2.5rem",
+          marginBottom: "20px",
+          opacity: 0,
+          transform: "translateY(20px)",
+          transition: "opacity 0.8s ease-in-out, transform 0.8s ease-in-out",
+          "&.appear": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        }}
+        className={animationClass}
+      >
+        ¡Bienvenido a nuestro sistema educativo!
+      </Typography>
+      <Typography
+        sx={{
+          opacity: 0,
+          transform: "translateY(20px)",
+          transition: "opacity 0.8s ease-in-out, transform 1.4s ease-in-out",
+          "&.appear": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        }}
+        className={animationClass}
+      >
+        Explora todas las características de nuestro sistema utilizando JSON Web
+        Tokens.
+      </Typography>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        <TextField
+          label="Email"
+          variant="outlined"
+          sx={{
+            marginBottom: "10px",
+            backgroundColor: "gray",
+            color: "#fff",
+            transform: "translateY(20px)",
+            transition: "opacity 0.8s ease-in-out, transform 1.8s ease-in-out",
+            "&.appear": {
+              transform: "translateY(0)",
+            },
+          }}
+          value={credentials.email}
+          className={animationClass}
+          onChange={(e) =>
+            setCredentials({ ...credentials, email: e.target.value })
+          }
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          sx={{
+            marginBottom: "10px",
+            color: "#fff",
+            backgroundColor: "gray",
+            transform: "translateY(20px)",
+            transition: "opacity 0.8s ease-in-out, transform 2.0s ease-in-out",
+            "&.appear": {
+              transform: "translateY(0)",
+            },
+          }}
+          type="password"
+          value={credentials.password}
+          className={animationClass}
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </Container>
+  );
 };
 
 export default LoginPage;
--- frontend/src/pages/welcomePage.jsx
+++ frontend/src/pages/welcomePage.jsx
@@ -1,111 +1,11 @@
-import {
-  Container,
-  Typography,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-  Button,
-  TextField,
-} from "@mui/material";
+import {} from "@mui/material";
 import { useState } from "react";
 
 const Welcome = () => {
-  const [alumnos, setAlumnos] = useState([
-    { nombre: "Juan", materia: "Matemáticas", nota: 90 },
-    { nombre: "Ana", materia: "Ciencias", nota: 95 },
-    // ...
-  ]);
-
-  const [nuevoAlumno, setNuevoAlumno] = useState({
-    nombre: "",
-    materia: "",
-    nota: 0,
-  });
-
-  const agregarAlumno = () => {
-    setAlumnos([...alumnos, nuevoAlumno]);
-    setNuevoAlumno({ nombre: "", materia: "", nota: 0 });
-  };
-
-  const eliminarAlumno = (nombre) => {
-    setAlumnos(alumnos.filter((alumno) => alumno.nombre !== nombre));
-  };
-
-  const handleChangeNuevoAlumno = (field, value) => {
-    setNuevoAlumno((prev) => ({ ...prev, [field]: value }));
-  };
-
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        background: `
-    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${process.env.PUBLIC_URL}/fotoLandingPage.jpg)
-  `,
-        backgroundSize: "cover",
-        color: "#fff",
-        textAlign: "center",
-        minWidth: "100%",
-      }}
-    >
-      <Typography>
-        En esta sección, vas a poder cargarle notas a tus alumnos.
-      </Typography>
-      <TextField
-        label="Nombre del alumno"
-        value={nuevoAlumno.nombre}
-        onChange={(e) => handleChangeNuevoAlumno("nombre", e.target.value)}
-      />
-      <TextField
-        label="Materia"
-        value={nuevoAlumno.materia}
-        onChange={(e) => handleChangeNuevoAlumno("materia", e.target.value)}
-      />
-      <TextField
-        type="number"
-        label="Nota"
-        value={nuevoAlumno.nota}
-        onChange={(e) =>
-          handleChangeNuevoAlumno("nota", parseInt(e.target.value, 10))
-        }
-      />
-      <Button onClick={agregarAlumno}>Agregar alumno</Button>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Nombre del alumno</TableCell>
-              <TableCell>Materia</TableCell>
-              <TableCell>Nota</TableCell>
-              <TableCell>Acciones</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {alumnos.map((alumno) => (
-              <TableRow key={alumno.nombre}>
-                <TableCell>{alumno.nombre}</TableCell>
-                <TableCell>{alumno.materia}</TableCell>
-                <TableCell>{alumno.nota}</TableCell>
-                <TableCell>
-                  <Button onClick={() => eliminarAlumno(alumno.nombre)}>
-                    Eliminar
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+    <div>
+      <h1>Bienvenido</h1>
+    </div>
   );
 };
 
